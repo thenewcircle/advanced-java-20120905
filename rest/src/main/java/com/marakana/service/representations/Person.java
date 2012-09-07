7c515c24509fe15dcdb2b01d76265b1bd567bba6
Dan Rosen <dan@marakana.com>
2012-09-07 15:49:47 -0700

    demonstrated Jackson serialization of an object

---
+++ rest/src/main/java/com/marakana/service/representations/Person.java
@@ -0,0 +1,51 @@
+package com.marakana.service.representations;
+
+public class Person {
+	private final String first;
+	private final String last;
+
+	public Person(String first, String last) {
+		this.first = first;
+		this.last = last;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + ((last == null) ? 0 : last.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Person))
+			return false;
+		Person other = (Person) obj;
+		if (first == null) {
+			if (other.first != null)
+				return false;
+		} else if (!first.equals(other.first))
+			return false;
+		if (last == null) {
+			if (other.last != null)
+				return false;
+		} else if (!last.equals(other.last))
+			return false;
+		return true;
+	}
+
+}
--- rest/src/main/java/com/marakana/service/resources/Hello.java
+++ rest/src/main/java/com/marakana/service/resources/Hello.java
@@ -2,13 +2,22 @@ package com.marakana.service.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@Path("/")
+import com.marakana.service.representations.Person;
+
+@Path("/hello")
 public class Hello {
 
 	@GET
-	public String sayHello() {
-		return "Hello.";
+	@Path("{first}/{last}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Person sayHello(
+			@PathParam("first") String first,
+			@PathParam("last") String last) {
+		return new Person(first, last);
 	}
 
 }
