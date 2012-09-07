73318f78b9b7b152e6584816710ce00e5d7c25a3
Dan Rosen <dan@marakana.com>
2012-09-07 10:19:17 -0700

    added new client/server project

---
+++ clientserver/src/main/java/com/marakana/clientserver/Server.java
@@ -0,0 +1,13 @@
+package com.marakana.clientserver;
+
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
